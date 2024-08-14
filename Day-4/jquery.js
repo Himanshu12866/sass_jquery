a2cc7ff8898053fd9bf67713582843163e14b535
Himanshu12866 <manshu010m@gmail.com>
2024-08-14 16:23:08 +0530

    "Updated index.htm to include Bootstrap Icons CSS, and modified jquery.js to change product card layout and add event listener for 'Add To Cart' button"

--- Day-4/jquery.js
+++ Day-4/jquery.js
@@ -7,12 +7,21 @@ $(function(){
            $(` <div class ="card p-2 m-2 w-25">
             <img src="${product.image}" width="100%" height="200px">
             <div class="card-header">
-            <p style="font-size:10px">${product.price.toLocaleString('en-in', {"style":"currency","currency":"INR"})}</p>
+            <p style="font-size:15px">${product.price.toLocaleString('en-in', {"style":"currency","currency":"INR"})}</p>
             </div>
             <div class="card-body">
-            <h6>${product.title}</h6>
+            <p class="bi bi-star-fill">${product.rating.rate} Ratings & ${product.rating.count} Reviews</p>
+            <i>${product.title}</i>
+            <br>
+            <b>${product.category.toUpperCase()}</b>
+            </div>
+            <div class="card-footer">
+            <button  value=${product.id} id="btn-id" class="btn btn-dark w-100">Add To Cart</button>
             </div>
             </div>`).appendTo("main")
       }  )
+    })
+    $(document).on("click" , "#btn-id" , (e) =>{
+        alert(`Product Id : ${e.target.value} : Added to the Card`)
     })
 })
