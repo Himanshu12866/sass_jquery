bc8732e8edb19f4018b7ec9042c248154e1b0550
Himanshu12866 <manshu010m@gmail.com>
2024-08-16 18:06:19 +0530

    "Added jquery.cookie.js script and implemented signout functionality with cookie removal and page redirect"

--- Day-8/jquery.js
+++ Day-8/jquery.js
@@ -3,3 +3,9 @@ $(() => {
     $("#dept").datepicker();
     $("#faqs").accordion()
 })
+$("#btnSignout").on("click" ,()=>{
+    alert("Do want to Signout ?")
+    $.removeCookie("name");
+    $.removeCookie("pass");
+    location.href ="../Day-9/index.html"
+})
