499205872e33a3a89cc2252a337461104872c99c
Himanshu12866 <manshu010m@gmail.com>
2024-08-17 17:45:41 +0530

    Modified

---
+++ Day-10/script.ts
@@ -0,0 +1,467 @@
+const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}
+const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}const REGEXES = [ /* ... */ ];
+
+function parseCode(code: string) {
+  const tokens = tokenize(code);
+  const syntaxTree = parse(tokens);
+
+  syntaxTree.forEach((node) => {
+    // parse...
+  });
+}
+
+function tokenize(code: string): Token[] {
+  const statements = code.split(' ');
+  const tokens: Token[] = [];
+
+  REGEXES.forEach((regex) => {
+    statements.forEach((statement) => {
+      tokens.push( /* ... */ );
+    });
+  });
+
+  return tokens;
+}
+
+function parse(tokens: Token[]): SyntaxTree {
+  const syntaxTree: SyntaxTree[] = [];
+  tokens.forEach((token) => {
+    syntaxTree.push( /* ... */ );
+  });
+
+  return syntaxTree;
+}
